8cab9f84b6b0bc57e76f166081c9e02097307e1d
jander5en <jonas341278@gmail.com>
2023-12-26 11:27:04 +0000

    add vundle to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ln -s ~/.vim/.vimrc ~/.vimrc 
-#heihei
 
+#installer packet manager Vundle(https://github.com/VundleVim/Vundle.vim):
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#set vim as editor med kommentar i bashrc
 echo "#Set up VIM as default editor" >> ~/.bashrc
 echo export VISUAL=vim >> ~/.bashrc
 echo export EDITOR=vim >> ~/.bashrc
-
 echo alias editor=vim >> ~/.bashrc
